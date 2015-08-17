5b0dfa50b7f252bb378d2a708fb7af8a93119d78
lopoly <lopoly@gmail.com>
2015-08-17 11:33:53 +0300

    videostream app

---
+++ app/src/main/java/ua/lopoly/videostreaming/AppConfig.java
@@ -0,0 +1,10 @@
+package ua.lopoly.videostreaming;
+
+/**
+ * Created by lopoly on 15.08.2015.
+ */
+public class AppConfig {
+    public static final String STREAM_URL = "rtsp://192.168.1.227:1935/live/myStream";
+    public static final String PUBLISHER_USERNAME = "lopoly";
+    public static final String PUBLISHER_PASSWORD = "skripka77";
+}
